e8573c77fa3892f1d9f748111b55d3b625c78992
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2017-03-17 08:13:01 -0500

    Remover imports de librerias que no se utilizan

--- Project/app/views.py
+++ Project/app/views.py
@@ -1,7 +1,3 @@
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
 from django.views import generic
 
 
