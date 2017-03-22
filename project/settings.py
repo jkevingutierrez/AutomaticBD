54073b536e8d956fee6abf4d9b53388959726394
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2017-03-22 01:33:26 -0500

    Modificar directorio de staticfiles

--- project/settings.py
+++ project/settings.py
@@ -130,8 +130,4 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
