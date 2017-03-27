06290e7297f68b17e4f485443b657602a7be5636
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2017-03-26 21:10:22 -0500

    Modificar mensajes de error

--- app/static/javascript/app/services/message.js
+++ app/static/javascript/app/services/message.js
@@ -12,14 +12,14 @@
      */
     function MessagesService(alertify) {
         this.error = function(error) {
-            alertify.closeLogOnClick(true)
-                .delay(6000)
+            alertify.closeLogOnClick(false)
+                .delay(5000)
                 .error('<div class="message-wrapper"><div class="message-icon"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></div><div class="message">' + error + '</div></div>');
         };
 
         this.success = function(message) {
-            alertify.closeLogOnClick(true)
-                .delay(6000)
+            alertify.closeLogOnClick(false)
+                .delay(5000)
                 .success('<div class="message-wrapper"><div class="message-icon"><span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></div><div class="message">' + message + '</div></div>');
         };
     }
