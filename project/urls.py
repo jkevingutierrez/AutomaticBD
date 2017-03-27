eabc2ed0f3d3c4f0384d619bc1df856000c050f0
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2017-03-27 11:44:37 -0500

    Modificar urls

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from app.views import NotFoundView
 
 handler404 = NotFoundView.as_view()
@@ -6,3 +8,7 @@ handler404 = NotFoundView.as_view()
 urlpatterns = [
     url(r'', include('app.urls'))
 ]
+
+urlpatterns = [
+    url(r'', include('app.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
