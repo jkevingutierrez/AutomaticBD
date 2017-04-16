d232363ce6959e11df7a76ff568a1ca6317431c9
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2017-04-16 13:40:00 -0500

    Remover minifyhtml

--- project/settings.py
+++ project/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     # 'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'htmlmin.middleware.HtmlMinifyMiddleware',
-    'htmlmin.middleware.MarkRequestMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
